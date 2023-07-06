e2f9db103da8f6488586750d0d8a5182152442c7
Yeedi0218 <106219401+Yeedi0218@users.noreply.github.com>
2023-07-06 20:39:05 +0800

    Update ex-1.cpp

--- ex-1.cpp
+++ ex-1.cpp
@@ -2,11 +2,16 @@
 
 int main(void)
 {
-    int i = 1;
+    int i=1;
+    int a,b;
     float f = 0.1;
-    char c = 'A'
-    printf(" hello Selsna ")
+    char c = 'A';
+    a = 20;
+    b = 50;
+    printf("hello Selsna ");
     printf("my height is %d m %f cm \n ", i , f);
-    printf(" in my last name , its first letter is %c \n", c)
+    printf("in my last name , its first letter is %c \n", c);
+    printf("a + b = %d \n",a+b); //+ , - , * , / , mod , %
+    
     return 0;
 }
