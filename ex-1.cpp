c6e8c4b5f3596cc6702b58c86be00a721cedffb4
Yeedi0218 <106219401+Yeedi0218@users.noreply.github.com>
2023-07-06 20:33:58 +0800

    Update ex-1.cpp

--- ex-1.cpp
+++ ex-1.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
-
-int main()
+int main(void)
 {
-    cout<<"Hello World";
-
+    int i = 1;
+    float f = 0.1;
+    char c = 'A'
+    printf(" hello Selsna ")
+    printf("my height is %d m %f cm \n ", i , f);
+    printf(" in my last name , its first letter is %c \n", c)
     return 0;
 }
